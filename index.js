a957c0dbc24fd06b4f497f382587f3a413f3beb5
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-10-27 08:53:21 +0600

    responses added

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const cors = require('cors')
 app.use(cors())
 
 const port = process.env.PORT || 5000;
+
+const allCourses = require('./data/courses.json')
+
+app.get('/', (req, res) => {
+    res.send('courses api is running')
+})
+
+app.get('/courses', (req, res)=> {
+    res.send(allCourses)
+})
+
+app.get('courses/:id', (req, res)=>{
+    const id = req.params.id 
+    const selectedCourse = allCourses.find(n => n.id == id)
+    res.send(selectedCourse)
+})
